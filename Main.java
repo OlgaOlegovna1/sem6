706538daa241c9f1cfd2b3215dda4e06a2286486
Olga Bel <olza91@mail.ru>
2023-03-26 10:51:36 +0300

    java-oop6

---
+++ Main.java
@@ -0,0 +1,13 @@
+пакет  Seminar05_Phonebook_mvc ;
+
+        импортировать  java . ио . IOException ;
+
+        общественный  класс  Основной {
+public  static  void  main ( String [] args ) выдает  IOException {
+
+        Контроллер  c = новый  Контроллер ();
+        в . начать ();
+
+
+        }
+        }
